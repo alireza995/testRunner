73e43a49566332f303f43948e4dba18c56a4464e
alireza995 <ma.sayedbagheri@gmail.com>
2023-12-30 15:04:40 +0330

    initial push

---
+++ testRunner/Caller.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace testRunner;
+
+public class Caller
+{
+    public static async Task<long> NewApiWithSemaphore(HttpClient httpClient, StringContent content, int index)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        Console.WriteLine($"number {index + 1} started");
+        var res = 
+            await httpClient.PostAsync("http://localhost:5295/Test/NewApiWithSemaphore/" + index, content);
+        
+        sw.Stop();
+        Console.WriteLine($"number {index + 1} finished in {sw.ElapsedMilliseconds} ms");
+
+        return res.IsSuccessStatusCode ? sw.ElapsedMilliseconds : 0;
+    }
+}
---
+++ testRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using testRunner;
+
+while (true)
+{
+    Console.WriteLine("enter count of tests:");
+    var count = int.Parse(Console.ReadLine()!);
+
+    var httpClient = new HttpClient();
+    var reader = new StreamReader(@"C:\Users\Radshid\Desktop\New folder\asd.txt");
+    var content = new StringContent(reader.ReadToEnd(), Encoding.UTF8, "application/json");
+
+    var newWithSemaphoreElapsedTime = new List<long>();
+
+    for (var i = 0; i < count; i++)
+        newWithSemaphoreElapsedTime.Add(await Caller.NewApiWithSemaphore(httpClient, content, i));
+
+    Console.WriteLine("avgWithSemaphore:" + newWithSemaphoreElapsedTime.Average());
+}
